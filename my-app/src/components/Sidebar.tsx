00da0e07f7881145b71da5b1f623dfe5cd1a5eb4
annaliao <jdoganna0620@yahoo.com.tw>
2024-10-20 10:07:15 +0800

    v1

--- my-app/src/components/Sidebar.tsx
+++ my-app/src/components/Sidebar.tsx
@@ -20,7 +20,61 @@ const Sidebar: React.FC<SidebarProps> = ({ currentBoard, switchBoard }) => {
         className={currentBoard === 'tech' ? 'active' : ''}
         onClick={() => handleSwitchBoard('tech', '/techboard')} // 切換到技術討論版
       >
-        技術討論版
+        任務達人秀
+      </button>
+      <button
+        className={currentBoard === 'life' ? 'active' : ''}
+        onClick={() => handleSwitchBoard('life', '/lifeboard')} // 切換到生活雜談版
+      >
+        閃耀時刻秀場
+      </button>
+      <button
+        className={currentBoard === 'life' ? 'active' : ''}
+        onClick={() => handleSwitchBoard('life', '/lifeboard')} // 切換到生活雜談版
+      >
+        感謝暖心牆
+      </button>
+      <button
+        className={currentBoard === 'life' ? 'active' : ''}
+        onClick={() => handleSwitchBoard('life', '/lifeboard')} // 切換到生活雜談版
+      >
+        公司神助攻
+      </button>
+      <button
+        className={currentBoard === 'life' ? 'active' : ''}
+        onClick={() => handleSwitchBoard('life', '/lifeboard')} // 切換到生活雜談版
+      >
+        星座職場指南
+      </button>
+      <button
+        className={currentBoard === 'life' ? 'active' : ''}
+        onClick={() => handleSwitchBoard('life', '/lifeboard')} // 切換到生活雜談版
+      >
+        團隊目標旗幟
+      </button>
+      <button
+        className={currentBoard === 'life' ? 'active' : ''}
+        onClick={() => handleSwitchBoard('life', '/lifeboard')} // 切換到生活雜談版
+      >
+        能量補給站
+      </button>
+      <button
+        className={currentBoard === 'life' ? 'active' : ''}
+        onClick={() => handleSwitchBoard('life', '/lifeboard')} // 切換到生活雜談版
+      >
+        團建嗨翻天日記
+      </button>
+      <button
+        className={currentBoard === 'life' ? 'active' : ''}
+        onClick={() => handleSwitchBoard('life', '/lifeboard')} // 切換到生活雜談版
+      >
+        DEI行動隊報告
+      </button>
+      <button
+        className={currentBoard === 'life' ? 'active' : ''}
+        onClick={() => handleSwitchBoard('life', '/lifeboard')} // 切換到生活雜談版
+      >
+        愛情與職場平衡術
       </button>
       <button
         className={currentBoard === 'life' ? 'active' : ''}
